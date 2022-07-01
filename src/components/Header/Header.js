820ba4b9921e4f68f49c83c99232ccd7d72db955
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-01 19:53:13 +0200

    fix logo functionality on click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,36 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightToBracket, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import {useNavigate} from "react-router-dom";
+
+import {faArrowRightToBracket, faUserPlus} from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "../Navbar/Navbar";
 
 import "./Header.scss";
 
 const Header = () => {
-    return (
-        <header className="header section__padding">
-            <div className="header__container">
-                <span className="header__logo">
-                    <span>Mi</span>
-                    <span>Casa</span>
-                </span>
-                <div className="header__controls">
-                    <button>
-                        <FontAwesomeIcon icon={faUserPlus} />
-                        Register
-                    </button>
-                    <button>
-                        <FontAwesomeIcon icon={faArrowRightToBracket} />
-                        Login
-                    </button>
-                </div>
-            </div>
-            <Navbar />
-        </header>
-    );
+	const navigate = useNavigate();
+
+	return (
+		<header className='header section__padding'>
+			<div className='header__container'>
+				<span className='header__logo' onClick={() => navigate("/")}>
+					<span>Mi</span>
+					<span>Casa</span>
+				</span>
+				<div className='header__controls'>
+					<button>
+						<FontAwesomeIcon icon={faUserPlus} />
+						Register
+					</button>
+					<button>
+						<FontAwesomeIcon icon={faArrowRightToBracket} />
+						Login
+					</button>
+				</div>
+			</div>
+			<Navbar />
+		</header>
+	);
 };
 
 export default Header;
