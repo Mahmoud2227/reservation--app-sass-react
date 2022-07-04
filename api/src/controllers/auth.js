3353b03e32827785af959aa59601955df648dcda
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-04 20:35:03 +0200

    edit register route and implement login route

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -2,10 +2,20 @@ import User from "../models/User.js";
 
 export const registerUser = async (req, res, next) => {
 	try {
-		const newUser = new User(req.body);
-		const token = await newUser.generateAuthToken();
-		await newUser.save();
-		res.status(201).json({newUser, token});
+		const user = new User(req.body);
+		const token = await user.generateAuthToken();
+		await user.save();
+		res.cookie("token", token, {httpOnly: true}).status(201).json({user});
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const loginUser = async (req, res, next) => {
+	try {
+		const user = await User.findByCredentials(req.body.username, req.body.password);
+		const token = await user.generateAuthToken();
+		res.cookie("token", token, {httpOnly: true}).status(200).json({user});
 	} catch (error) {
 		next(error);
 	}
--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
 	username: {
@@ -27,6 +28,34 @@ userSchema.methods.generateAuthToken = async function () {
 	await user.save();
 	return token;
 };
+
+userSchema.methods.toJSON = function () {
+	const user = this;
+	const userObject = user.toObject();
+	delete userObject.password;
+	return userObject;
+};
+
+userSchema.statics.findByCredentials = async (username, password) => {
+	const user = await User.findOne({username});
+	if (!user) {
+		throw new Error("Unable to login");
+	}
+	const isMatch = bcrypt.compare(password, user.password);
+
+	if (!isMatch) {
+		throw new Error("Unable to login");
+	}
+	return user;
+};
+
+userSchema.pre("save", async function (next) {
+	const user = this;
+	if (user.isModified("password")) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
+	next();
+});
 
 const User = mongoose.model("User", userSchema);
 
