cb9cd93031cc1119f4716b647f8f5a850609ade3
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-04 23:55:51 +0200

    add isAdmin property to userSchema

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 
 export const getUserInfo = async (req, res, next) => {
 	try {
-		return res.status(200).json(req.user);
+		return res.status(200).send(req.user);
 	} catch (error) {
 		next(error);
 	}
--- api/src/models/User.js
+++ api/src/models/User.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
 		trim: true,
 		unique: true,
 	},
+	isAdmin: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 userSchema.methods.generateAuthToken = async function () {
