1de91be1874c374be1c495a74239e6282c33f247
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-05 21:17:38 +0200

    fix verifyAdmin middleware

--- api/src/middleware/verifyAdmin.js
+++ api/src/middleware/verifyAdmin.js
@@ -1,7 +1,7 @@
 import createError from "../utils/createError.js"
 
 const verifyAdmin = (req, res, next) => {
-  if (req.user.role !== "admin") {
+  if (!req.user.isAdmin) {
     return next(createError(403, "unauthorized"));
   }
   next();
