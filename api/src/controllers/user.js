dd9389bcc625f1faa6ee369ef4dc2bc4be9963ef
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-06 00:13:48 +0200

    implement update user info route

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import createError from "../utils/createError.js";
 
 export const getUserInfo = async (req, res, next) => {
 	try {
@@ -12,6 +13,25 @@ export const getAllUsersInfo = async (req, res, next) => {
 	try {
 		const users = await User.find({});
 		return res.status(200).send(users);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const UpdateUserInfo = async (req, res, next) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ["email", "password", "isAdmin"];
+	const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+	const isValidAdminUpdate = updates.includes("isAdmin") ? req.user.isAdmin : true;
+
+	try {
+		if (!isValidUpdate || !isValidAdminUpdate) {
+			next(createError(400, "Invalid updates!"));
+		} else {
+			updates.forEach((update) => (req.user[update] = req.body[update]));
+			await req.user.save();
+			return res.status(200).send(req.user);
+		}
 	} catch (error) {
 		next(error);
 	}
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getAllUsersInfo, getUserInfo} from "../controllers/user.js";
+import {getAllUsersInfo, getUserInfo, UpdateUserInfo} from "../controllers/user.js";
 import verifyAdmin from "../middleware/verifyAdmin.js";
 import verifyToken from "../middleware/verifyToken.js";
 import verifyUser from "../middleware/verifyUser.js";
@@ -8,6 +8,8 @@ const router = express.Router();
 
 router.get("/:id", verifyToken, verifyUser, getUserInfo);
 
-router.get("/", verifyToken, verifyAdmin, getAllUsersInfo)
+router.get("/", verifyToken, verifyAdmin, getAllUsersInfo);
+
+router.patch("/:id", verifyToken, verifyUser, UpdateUserInfo);
 
 export default router;
