4b16e675488eb78cbe293a3e45914fb92118a662
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-08 20:28:10 +0200

    implement get property by user id route

--- api/src/controllers/property.js
+++ api/src/controllers/property.js
@@ -1,10 +1,22 @@
 import Property from "../models/Property.js";
+import User from "../models/User.js";
 
 export const createProperty = async (req, res, next) => {
+	try {
+		const property = new Property({...req.body, owner: req.params.ownerId});
+		await property.save();
+		res.status(200).send(property);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getPropertyListByUser = async (req, res, next) => {
   try {
-    const property = new Property({...req.body,owner:req.params.ownerId})
-    await property.save();
-    res.status(200).send(property)
+    const user = await User.findById(req.params.ownerId)
+    await user.populate({path:"property"})
+    const propertyList = user.property
+    res.status(200).send(propertyList)
   } catch (error) {
     next(error)
   }
--- api/src/routes/property.js
+++ api/src/routes/property.js
@@ -2,10 +2,12 @@ import express from "express";
 
 import verifyToken from "../middleware/verifyToken.js";
 import verifyUser from "../middleware/verifyUser.js";
-import {createProperty} from "../controllers/property.js";
+import {createProperty, getPropertyListByUser} from "../controllers/property.js";
 
 const router = express.Router();
 
 router.post("/:ownerId", verifyToken, verifyUser, createProperty);
+
+router.get("/:ownerId", verifyToken, verifyUser, getPropertyListByUser);
 
 export default router;
