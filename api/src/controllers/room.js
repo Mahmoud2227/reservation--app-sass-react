9026d0e27a7623d5c076e51fac3d37ad90b98206
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-10 19:16:41 +0200

    edit create room controller structure

--- api/src/controllers/room.js
+++ api/src/controllers/room.js
@@ -1,11 +1,9 @@
 import Room from "../models/Room.js";
-import Property from "../models/Property.js"
 
 export const createRoom = async (req,res,next) => {
   try {
-    const room = new Room(req.body);
+    const room = new Room({...req.body,propertyId:req.params.propertyId});
     await room.save();
-    await Property.findByIdAndUpdate(req.params.propertyId,{$push:{rooms:room._id}})
     res.status(200).send(room)
   } catch (error) {
     next(error)
