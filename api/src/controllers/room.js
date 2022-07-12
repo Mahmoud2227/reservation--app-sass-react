02918b03034e1024b46d37c4ae1744a57b2c933d
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-12 18:45:44 +0200

    add get rooms by property route

--- api/src/controllers/room.js
+++ api/src/controllers/room.js
@@ -1,11 +1,23 @@
 import Room from "../models/Room.js";
+import Property from "../models/Property.js";
 
-export const createRoom = async (req,res,next) => {
-  try {
-    const room = new Room({...req.body,propertyId:req.params.propertyId});
-    await room.save();
-    res.status(200).send(room)
-  } catch (error) {
-    next(error)
-  }
-}
+export const createRoom = async (req, res, next) => {
+	try {
+		const room = new Room({...req.body, propertyId: req.params.propertyId});
+		await room.save();
+		res.status(200).send(room);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getRoomsByProperty = async (req, res, next) => {
+	try {
+		const property = await Property.findById(req.params.propertyId);
+		await property.populate({path: "rooms"});
+		const rooms = property.rooms;
+		res.status(200).send(rooms);
+	} catch (error) {
+		next(error);
+	}
+};
--- api/src/routes/room.js
+++ api/src/routes/room.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRoom } from "../controllers/room.js";
+import { createRoom, getRoomsByProperty } from "../controllers/room.js";
 
 import verifyToken from "../middleware/verifyToken.js"
 import verifyUser from "../middleware/verifyUser.js"
@@ -7,5 +7,7 @@ import verifyUser from "../middleware/verifyUser.js"
 const router = express.Router();
 
 router.post("/:id/:propertyId",verifyToken,verifyUser,createRoom)
+
+router.get("/:id/:propertyId",verifyToken,verifyUser,getRoomsByProperty);
 
 export default router
