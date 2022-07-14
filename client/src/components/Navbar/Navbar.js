f532541ca8fde0eede4fb07791a728437a33b66e
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-14 23:31:10 +0200

    change navbar link and add active styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,44 +1,45 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBed, faPlane, faCar, faTaxi } from "@fortawesome/free-solid-svg-icons";
+import {NavLink} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBed, faPlane, faCar, faTaxi} from "@fortawesome/free-solid-svg-icons";
 import "./Navbar.scss";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <ul className="navbar__links">
-                <li className="navbar__links-item">
-                    <a href="/stays">
-                        <FontAwesomeIcon icon={faBed} />
-                        Stays
-                    </a>
-                </li>
-                <li className="navbar__links-item">
-                    <a href="/flights">
-                        <FontAwesomeIcon icon={faPlane} />
-                        Flights
-                    </a>
-                </li>
-                <li className="navbar__links-item">
-                    <a href="/carRent">
-                        <FontAwesomeIcon icon={faCar} />
-                        Car rentals
-                    </a>
-                </li>
-                <li className="navbar__links-item">
-                    <a href="/attractions">
-                        <FontAwesomeIcon icon={faBed} />
-                        Attractions
-                    </a>
-                </li>
-                <li className="navbar__links-item">
-                    <a href="/taxis">
-                        <FontAwesomeIcon icon={faTaxi} />
-                        Airport taxis
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    );
+	return (
+		<nav className='navbar'>
+			<ul className='navbar__links'>
+				<li className='navbar__links-item'>
+					<NavLink className={(navData) => (navData.isActive ? "active" : "")} to='/stays'>
+						<FontAwesomeIcon icon={faBed} />
+						Stays
+					</NavLink>
+				</li>
+				<li className='navbar__links-item'>
+					<NavLink className={(navData) => (navData.isActive ? "active" : "")} to='/flights'>
+						<FontAwesomeIcon icon={faPlane} />
+						Flights
+					</NavLink>
+				</li>
+				<li className='navbar__links-item'>
+					<NavLink className={(navData) => (navData.isActive ? "active" : "")} to='/carRent'>
+						<FontAwesomeIcon icon={faCar} />
+						Car rentals
+					</NavLink>
+				</li>
+				<li className='navbar__links-item'>
+					<NavLink className={(navData) => (navData.isActive ? "active" : "")} to='/attractions'>
+						<FontAwesomeIcon icon={faBed} />
+						Attractions
+					</NavLink>
+				</li>
+				<li className='navbar__links-item'>
+					<NavLink className={(navData) => (navData.isActive ? "active" : "")} to='/taxis'>
+						<FontAwesomeIcon icon={faTaxi} />
+						Airport taxis
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
